72fcbc334f98dd272335945b2e691fe62142b140
Art <art@stoupin.ru>
2020-03-08 23:38:34 +0300

    ok

--- customization/execute.sh
+++ customization/execute.sh
@@ -12,6 +12,9 @@ JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
 JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
 
+
+echo var1 is ${var1}
+
 function wait_for_server() {
   until `$JBOSS_CLI -c ":read-attribute(name=server-state)" 2> /dev/null | grep -q running`; do
     sleep 1
@@ -36,15 +39,15 @@ $JBOSS_CLI -c --file=`dirname "$0"`/commands.cli
 
 
 
-# Deploy the WAR
-#cp /opt/jboss/wildfly/customization/leadservice-1.0.war $JBOSS_HOME/$JBOSS_MODE/deployments/leadservice-1.0.war
-
 echo "=> Shutting down WildFly"
 if [ "$JBOSS_MODE" = "standalone" ]; then
   $JBOSS_CLI -c ":shutdown"
 else
   $JBOSS_CLI -c "/host=*:shutdown"
 fi
+
+# Deploy the WAR
+cp /opt/jboss/wildfly/customization/*.war $JBOSS_HOME/$JBOSS_MODE/deployments
 
 echo "=> Restarting WildFly"
 $JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c $JBOSS_CONFIG
